fec28b8d98e6c28b367a40f51bfc0e811028a91e
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-08-13 14:27:46 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,7 +2,8 @@ var playlist = {
   philOchs: 'Changes'
 }
 function updatePlaylist (playlist, artistName, songTitle) {
-return(playlist[artistName] = songTitle)
+playlist[artistName] = songTitle
+return(playlist)
 }
 function removeFromPlaylist (playlist, artistName) {
 return(delete playlist.songTitle)
