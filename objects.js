a779ab38c8afb43ff7bfbc97e1d66425b5e2e617
Grant Stafford-Renard <gstaffordrenard@gmail.com>
2020-08-13 13:46:35 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+  tobyFox: 'megalovania',
+  nintendo: 'godShatteringStar',
+  splatoon: 'splatatack'
+}
+function updatePlaylist (playlist, artistName, songTitle) {
+
+}
+function removeFromPlaylist (playlist, artistName) {
+
+}
